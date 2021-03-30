49e10f60cbce8541cd1d3e7f113c2e3b6dd6a1dc
SettachaiPK <settachaipk@gmail.com>
2021-03-30 23:15:08 +0700

    db connection (mariadb)

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class MovieType(models.Model):
-    type = models.CharField(max_length=5)
+    type = models.CharField(max_length=10)
 
 
 class MovieTag(models.Model):
@@ -23,4 +23,3 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.name, self.price, self.rate
-
