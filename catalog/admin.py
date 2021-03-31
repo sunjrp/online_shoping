6fea76ce2a6839cbc7cba6290678127c55d51acf
Phicheteak <81636590+Phicheteak@users.noreply.github.com>
2021-03-31 21:25:52 +0700

    Add files via upload

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie,MovieType,MovieTag
+
+admin.site.register(Movie)
+admin.site.register(MovieType)
+admin.site.register(MovieTag)
 
 # Register your models here.
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from .models import *
-import time
-from django.db.models import Q
+from .models import Movie, MovieTag, MovieType
 from django.http import HttpResponse
 # Create your views here.
 
@@ -16,32 +14,10 @@ def market(request):
 
 def search(request):
     keyword = request.GET['keyword']
-    print(keyword)
-    start_time = time.time()
-    data = query_search(keyword)
-    print(data)
-    elapsed_time = time.time() - start_time
-    return render(request, 'result.html', {'query': data, 'query_time': elapsed_time})
-
-
-def query_search(keyword, date_select=None, type_select=None, tag_select=None, price_select=None):
-    """search data in DB by time and keyword and return query set"""
-    if type_select is None:
-        type_select = []
-    result = Movie.objects.all()
-
-    if date_select:
-        result = result.exclude(Q(release_date__gt=date_select[0]) | Q(release_date__lt=date_select[1]))
-
-    if keyword != "":  # if have specific keyword
-        result = result.filter(Q(name__startswith=keyword))
-        if price_select:
-            result = result.exclude(Q(price__gt=price_select[0]) | Q(price__lt=price_select[1]))
-        if type_select:
-            for word in type_select:
-                result = result.filter(Q(MovieType__type=word))
-        if tag_select:
-            for word in tag_select:
-                result = result.filter(Q(MovieType__type=word))
-
-    return result
+    db = request.GET['db']
+    print(keyword, db)
+    if db == "SQL":
+        data = Movie.objects.filter(name__icontains = keyword)
+    else:
+        data = None
+    return render(request, 'result.html', {'query': data})
