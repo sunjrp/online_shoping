d9f912dfadbf6d1aad9c7f03a620395def2cea4f
Phicheteak <81636590+Phicheteak@users.noreply.github.com>
2021-03-31 21:24:38 +0700

    Add files via upload

--- online_shoping/urls.py
+++ online_shoping/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from catalog import views
 
 urlpatterns = [
@@ -22,4 +24,6 @@ urlpatterns = [
     path('', views.home),
     path('market', views.market),
     path('search', views.search),
+
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
